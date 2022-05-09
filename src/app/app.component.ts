3543ca69e674f0fff3784b048727114ef58884ee
HelixFix <jerome.rinner@gmail.com>
2022-05-09 14:12:26 +0200

    Secure Angular Environment Variables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,25 @@
 import { Component } from '@angular/core';
 import { initializeApp } from "firebase/app";
+import { environment } from '../environments/environment';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor() {
+ constructor() {
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB2jAmfxfk4eXmL64W8BAxR_wNkY6LSHpA",
-  authDomain: "bookshelves-28a2a.firebaseapp.com",
-  projectId: "bookshelves-28a2a",
-  storageBucket: "bookshelves-28a2a.appspot.com",
-  messagingSenderId: "723787039894",
-  appId: "1:723787039894:web:31323a68daf4c81fa8eeb7"
+  apiKey           : environment.API_KEY,
+  authDomain       : environment.authDomain,
+  projectId        : environment.projectId,
+  storageBucket    : environment.storageBucket,
+  messagingSenderId: environment.messagingSenderId,
+  appId            : environment.appId
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-  }
+ }
 }
