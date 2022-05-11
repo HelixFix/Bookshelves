2e7e5b895d239fc08df1b1a36c34ca85ae462f0b
HelixFix <jerome.rinner@gmail.com>
2022-05-11 10:31:16 +0200

    Check header state auth

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import firebase from 'firebase/compat/app';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  isAuth!: Boolean;
 
+  constructor(private authService: AuthService) { }
+
+/**
+ * When the component is initialized, check if the user is authenticated and set the isAuth variable
+ * accordingly.
+ */
   ngOnInit(): void {
+    firebase.auth().onAuthStateChanged(
+      (user) => {
+        if(user) {
+          this.isAuth = true;
+        } else {
+          this.isAuth = false;
+        }
+      }
+    )
+  }
+
+/**
+ * It calls the signOutUser() function from the authService
+ */
+  onSignOut() {
+    this.authService.signOutUser();
   }
 
 }
